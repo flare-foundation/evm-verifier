e63f52830bcb3402a072b61ae115a12c92d3a7d3
alenabelium <33514602+alenabelium@users.noreply.github.com>
2023-10-09 00:46:07 +0200

    evm verifier implementation

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,7 @@
 export interface IConfig {
     port: number;
     api_keys: string[];
+    rpc: string;
 }
 
 export default () => {
@@ -8,6 +9,7 @@ export default () => {
     const config: IConfig = {
         port: parseInt(process.env.PORT || "3000"),
         api_keys,
+        rpc: process.env.RPC || "https://flare-api.flare.network/ext/C/rpc",
     };
     return config;
 };
