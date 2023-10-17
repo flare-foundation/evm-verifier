994bd95b5847cabc9385eac4276081aa0172ada8
grePod <gregor.podlogar@aflabs.si>
2023-10-17 17:17:35 +0200

    tests added

---
+++ src/test/evm-transaction-verifier.spec.ts
@@ -0,0 +1,87 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { EVMTransactionVerifierController } from "../controller/evm-transaction-verifier.controller";
+import { ConfigModule } from "@nestjs/config";
+import configuration from "../config/configuration";
+import { AuthModule } from "../auth/auth.module";
+import { ApiKeyStrategy } from "../auth/apikey.strategy";
+import { AuthService } from "../auth/auth.service";
+import { EVMTransactionVerifierService } from "../service/evm-transaction-verifier.service";
+import { resolve } from "url";
+import { AttestationResponseStatus } from "../external-libs/ts/AttestationResponse";
+
+const request = {
+    attestationType: "0x45564d5472616e73616374696f6e000000000000000000000000000000000000",
+    sourceId: "0x4554480000000000000000000000000000000000000000000000000000000000",
+    requestBody: {
+        transactionHash: "0xd71d5dd7ebb91ba7bd9d943973e3066666db430e56a34b225279872869fbeaf5",
+        requiredConfirmations: "1",
+        provideInput: true,
+        listEvents: false,
+        logIndices: ["0", "1", "2"],
+    },
+};
+
+describe("Verification correctness", () => {
+    let appController: EVMTransactionVerifierController;
+
+    beforeAll(async () => {
+        const app: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot({
+                    load: [configuration],
+                    isGlobal: true,
+                }),
+                AuthModule,
+            ],
+            controllers: [EVMTransactionVerifierController],
+            providers: [ApiKeyStrategy, AuthService, EVMTransactionVerifierService],
+        }).compile();
+
+        appController = app.get<EVMTransactionVerifierController>(EVMTransactionVerifierController);
+    });
+
+    test("should not verify the request with indices and list events false", async function () {
+        const res = await appController.prepareResponse(request);
+        expect(res.status).toBe(AttestationResponseStatus.INVALID);
+    });
+
+    test("should list all events if so specified ", async function () {
+        request.requestBody.listEvents = true;
+        request.requestBody.logIndices = [];
+
+        const res = await appController.prepareResponse(request);
+        expect(res.status).toBe(AttestationResponseStatus.VALID);
+        expect(res.response).toBeDefined();
+        expect(res.response?.responseBody.events.length).toBe(24);
+    });
+
+    test("should not verify if non-existent index", async function () {
+        request.requestBody.listEvents = true;
+        request.requestBody.logIndices = ["50"];
+
+        const res = await appController.prepareResponse(request);
+        expect(res.status).toBe(AttestationResponseStatus.INVALID);
+    });
+
+    test("should sort events according to logIndices", async function () {
+        request.requestBody.listEvents = true;
+        request.requestBody.logIndices = ["2", "17", "3", "1", "3"];
+
+        const res = await appController.prepareResponse(request);
+
+        expect(res.status).toBe(AttestationResponseStatus.VALID);
+
+        expect(res.response?.responseBody.events[0].logIndex).toBe("2");
+        expect(res.response?.responseBody.events[1].logIndex).toBe("17");
+        expect(res.response?.responseBody.events[4].logIndex).toBe("3");
+    });
+
+    test("should reject verify non-existent transaction", async function () {
+        request.requestBody.listEvents = false;
+        request.requestBody.logIndices = [];
+        request.requestBody.transactionHash = "0xc71d5dd7ebb91ba7bd9d943973e3066666db430e56a34b225279872869fbeaf5";
+
+        const res = await appController.prepareResponse(request);
+        expect(res.status).toBe(AttestationResponseStatus.INVALID);
+    });
+});
